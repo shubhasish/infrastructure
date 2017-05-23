088b282e179c938d5dac7e61b42ec5e32fb18146
root <panda.subhasish007@gmail.com>
2017-05-23 16:07:30 +0530

    First Commit

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "keyPath"{
+default = "/home/subhasishp/Downloads/linux3.pem"}
+
+variable "accessKey" {
+default = "AKIAI6AO2LYMYRUPRIFA"}
+
+variable "secretKey" {
+default = "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" }
+
+variable "region" {
+default = "ap-south-1" }
